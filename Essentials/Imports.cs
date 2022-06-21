563004912187c52b748857b2accc668b6ca842cf
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2022-06-21 13:41:52 +0200

    Update Imports.cs

--- Essentials/Imports.cs
+++ Essentials/Imports.cs
@@ -9,10 +9,15 @@ namespace LeagueAutoLogin.Essentials
 {
     class Imports
     {
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
         public static int MOUSEEVENTF_LEFTDOWN = 0x02;
         public static int MOUSEEVENTF_LEFTUP = 0x04;
+
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
 
